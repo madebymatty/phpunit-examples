d78a639ad8e262d19060dba470dd2c52d4134f4d
Matt Woods <mjaywoods@gmail.com>
2017-12-12 09:50:23 +0000

    Commit a failing test, for testing purposes

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -6,7 +6,7 @@ class SimpleTest extends \PHPUnit_Framework_TestCase
 	public function testDivide()
 	{
 		$simple = new Simple(10);
-		$result = $simple->divide(2);
+		$result = $simple->divide(3);
 
 		$this->assertEquals(5, $result);
 	}
